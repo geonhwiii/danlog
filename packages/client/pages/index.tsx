59ba29b9e1784eb36bac079159870a7e41d6b207
geonhwiii <g01063962671@gmail.com>
2023-02-13 01:45:47 +0900

    add workspace client

---
+++ packages/client/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import { Button } from '@danlog/common';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Danlog</title>
+        <meta name="description" content="danlog" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Button />
+      </main>
+    </>
+  );
+}
