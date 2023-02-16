e0fb765d293292f2fe4ee4b0650e7d2267de2f03
Dan <geonhwi@github.com>
2023-02-16 21:02:15 +0900

    add recent posts

--- packages/client/pages/daily/index.tsx
+++ packages/client/pages/daily/index.tsx
@@ -1,7 +1,16 @@
+import DailyCard from '@/components/organisms/DailyCard';
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
 
 export default function DailyLayout({}: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <div>DailyLayout</div>;
+  return (
+    <div>
+      <DailyCard />
+      <DailyCard />
+      <DailyCard />
+      <DailyCard />
+      <DailyCard />
+    </div>
+  );
 }
 
 export const getStaticProps: GetStaticProps = () => {
--- packages/client/pages/post/index.tsx
+++ packages/client/pages/post/index.tsx
@@ -1,7 +1,16 @@
+import PostCard from '@/components/organisms/PostCard';
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
 
 export default function PostLayout({}: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <div>PostLayout</div>;
+  return (
+    <div>
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+    </div>
+  );
 }
 
 export const getStaticProps: GetStaticProps = () => {
