1b003f9cba434edf7948b4059175dc82c9f82ed6
Dan <geonhwi@github.com>
2023-02-21 16:21:47 +0900

    remove cache provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,13 @@
 import type { AppProps } from 'next/app';
-import { cache } from '@emotion/css';
-import { CacheProvider } from '@emotion/react';
 import Layout from '@/components/templates/Layout';
 import GlobalStyles from '@/styles/GlobalStyles';
 import '@/styles/prism.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <CacheProvider value={cache}>
-      <Layout>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </Layout>
-    </CacheProvider>
+    <Layout>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </Layout>
   );
 }
